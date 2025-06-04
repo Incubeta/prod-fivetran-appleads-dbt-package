a0f81b70003b68fa26e3372a768b1c1076d54401
avinashk0 <avinashk0@gmail.com>
2025-06-04 14:34:30 +0100

    Removed the days back logic

--- models/apple/adgroup_performance/marts/pub_base-appleads-adgroup_performance.sql
+++ models/apple/adgroup_performance/marts/pub_base-appleads-adgroup_performance.sql
@@ -6,9 +6,6 @@
   )
 }}
 
-{% set region = bq_timezone('target_region') %}
-{% set dates = var('days_back').split(',') %}
-
 with report as (
   select *
   from {{ref('int_adgroup_performance')}}
@@ -18,9 +15,3 @@ select
   {{dbt_utils.generate_surrogate_key(["day", "adgroup_id", "campaign_id"])}} as unique_id,
   report.*
 from report
-where
-{% if var('first_run') == default_variable("first_run_value1") %}
-  day IN {{ days_back(dates,region) }}
-{% elif var('first_run') == default_variable("first_run_value2") %}
-  day > SAFE_cast({{ default_variable("first_run_date") }} as date)
-{% endif %}
